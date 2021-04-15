8b5eec744c74b3b337d801730f71f702b086a0f7
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-15 11:01:58 +0400

    update StatsService

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -13,6 +13,7 @@ class StatisticsServiceTest {
 
         int actual = service.calculateSum(purchases);
         assertEquals(expected, actual);
+        System.out.println("Сумма всех продаж: " + actual);
     }
 
     @Test
@@ -23,6 +24,7 @@ class StatisticsServiceTest {
 
         int actual = service.calculateAverage(purchases);
         assertEquals(expected, actual);
+        System.out.println("Средняя сумма продаж в месяц: " + actual);
     }
 
     @Test
@@ -33,6 +35,7 @@ class StatisticsServiceTest {
 
         int actual = service.findMax(purchases);
         assertEquals(expected, actual);
+        System.out.println("Максимум продаж: " + actual);
     }
 
     @Test
@@ -43,16 +46,18 @@ class StatisticsServiceTest {
 
         int actual = service.findMin(purchases);
         assertEquals(expected, actual);
+        System.out.println("Минимум продаж: " + actual);
     }
 
     @Test
-    void calculateBelowAverage() {
+     void calculateBelowAverageSales() {
         StatsService service = new StatsService();
         int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
 
         int actual = service.calculateBelowAverageSales(purchases);
         assertEquals(expected, actual);
+        System.out.println("Продажи ниже среднего: " + actual);
     }
 
     @Test
@@ -63,5 +68,6 @@ class StatisticsServiceTest {
 
         int actual = service.calculateAboveAverageSales(purchases);
         assertEquals(expected, actual);
+        System.out.println("Продажи выше среднего: " + actual);
     }
 }
